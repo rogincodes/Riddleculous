1f1bbca53b1478dafa90fdab1101aff99c2b6956
rogincodes <cartallarogin@gmail.com>
2024-09-13 09:53:34 +0800

    modified port and header title

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 var riddleAnswer = "";
 
 app.use(express.static("public"));
