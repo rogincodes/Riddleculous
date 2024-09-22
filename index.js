292af35f900b882c22838c320bb2eb9d844cc464
rogincodes <cartallarogin@gmail.com>
2024-09-23 01:51:35 +0800

    add comments, and add compliments for correct answer

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 var riddleAnswer = "";
 
+// Middleware
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// GET index.ejs
 app.get("/", (req, res) => {
 	res.render("index.ejs");
 });
 
+// POST submit name
 app.post("/submitName", async (req, res) => {
 	try {
 		res.render("index.ejs", { name: req.body.name });
@@ -22,6 +25,7 @@ app.post("/submitName", async (req, res) => {
 	}
 });
 
+// GET random riddle
 app.get("/riddle.ejs", async (req, res) => {
 	try {
 		const result = await axios.get("https://riddles-api.vercel.app/random");
@@ -35,25 +39,29 @@ app.get("/riddle.ejs", async (req, res) => {
 	}
 });
 
+// GET goodbye.ejs
 app.get("/goodbye.ejs", (req, res) => {
   res.render("goodbye.ejs");
 });
 
+// POST submit answer
 app.post("/submitAnswer", async (req, res) => {
 	try {
-		const result = await axios.get("https://evilinsult.com/generate_insult.php?lang=en&type=json");
+		const insultAPI = await axios.get("https://evilinsult.com/generate_insult.php?lang=en&type=json");
+		const complimentAPI = await axios.get("http://localhost:3000/compliments/random")
 		let userAnswer = req.body.answer.toUpperCase();
 		const upperCasedAnswer = riddleAnswer.toUpperCase();
 		let words = userAnswer.split(" ");
 		let found = words.some(word => upperCasedAnswer.includes(word));
 		
+		// check answer		
 		if (userAnswer === "") {
 			res.render("riddle.ejs", { answer: `The correct answer is "${riddleAnswer}".`, verdict: result.data.insult })
 		} else {
 			if (found) {
-				res.render("riddle.ejs", { answer: `The correct answer is "${riddleAnswer}".`, verdict: "Congratulations on not getting insulted!"});
+				res.render("riddle.ejs", { answer: `The correct answer is "${riddleAnswer}".`, verdict: complimentAPI.data.compliment, textColor: "green-Text" });
 			} else {
-				res.render("riddle.ejs", { answer: `The correct answer is "${riddleAnswer}".`, verdict: result.data.insult })
+				res.render("riddle.ejs", { answer: `The correct answer is "${riddleAnswer}".`, verdict: insultAPI.data.insult, textColor: "red-Text" });
 			}
 		}	
 	} catch (error) {
